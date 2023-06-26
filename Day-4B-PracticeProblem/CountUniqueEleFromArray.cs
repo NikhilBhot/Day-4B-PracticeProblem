e93582a18131055ba0e25b1f4a42f94a19efc8cc
unknown <bhotnikhil25@gmail.com>
2023-06-26 10:23:38 +0530

    [Nikhil]-Find Unique Elemet From Array

---
+++ Day-4B-PracticeProblem/CountUniqueEleFromArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4B_PracticeProblem
+{
+    //Write a program in C# Sharp to print all unique elements in an array.
+    public class CountUniqueEleFromArray
+    {
+        public static bool IsUnique(int[] arr, int num)
+        {
+            int count = 0;
+            foreach (var element in arr)
+            {
+                if (element == num)
+                {
+                    count++;
+                    if (count > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- Day-4B-PracticeProblem/Program.cs
+++ Day-4B-PracticeProblem/Program.cs
@@ -9,6 +9,18 @@
             int[] arr = { 1, 2, 3, 4, 5, 2, 4, 6, 7, 3, 8, 4, 9, 1 };
             int duplicateCount = CountDulpicateEleFromArray.CountDuplicateElements(arr);
             Console.WriteLine("Total number of duplicate elements: " + duplicateCount);
+            Console.WriteLine("_________________________________________");
+            Console.WriteLine("Find Unique Element From Array");
+            int[] arr1 = { 1, 2, 3, 4, 5, 2, 4, 6, 7, 3, 8, 4, 9, 1 };
+
+            Console.WriteLine("Unique elements in the array:");
+            foreach (var element in arr1)
+            {
+                if (CountUniqueEleFromArray.IsUnique(arr1, element))
+                {
+                    Console.WriteLine(element);
+                }
+            }
         }
     }
 }
