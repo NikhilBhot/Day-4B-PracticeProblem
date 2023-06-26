72fa46c920b1b639bedea6957a5324f2e060626a
unknown <bhotnikhil25@gmail.com>
2023-06-26 10:12:02 +0530

    [Nikhil]-Count The Total Duplicate Array Element

---
+++ Day-4B-PracticeProblem/CountDulpicateEleFromArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4B_PracticeProblem
+{
+    public class CountDulpicateEleFromArray
+    {
+        public static int CountDuplicateElements(int[] arr)
+        {
+            int duplicateCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        duplicateCount++;
+                        break;
+                    }
+                }
+            }
+            return duplicateCount;
+        }
+    }
+}
---
+++ Day-4B-PracticeProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_4B_PracticeProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Find duplicate Elements from Array ");
+            int[] arr = { 1, 2, 3, 4, 5, 2, 4, 6, 7, 3, 8, 4, 9, 1 };
+            int duplicateCount = CountDulpicateEleFromArray.CountDuplicateElements(arr);
+            Console.WriteLine("Total number of duplicate elements: " + duplicateCount);
+        }
+    }
+}
